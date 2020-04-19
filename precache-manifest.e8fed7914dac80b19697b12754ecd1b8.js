aafb0d68f5d03bacba3e7d4caa2a8494b7c6d3f1
akintomiwa fisayo <akintomiwa.fisayo@gmail.com>
2020-04-19 10:51:20 +0100

    Updates

---
+++ precache-manifest.e8fed7914dac80b19697b12754ecd1b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a7f0fa5a2c559e4fec2fc19b6b418a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a7da2dea8bebee0a04",
+    "url": "/static/css/2.54e6f0dc.chunk.css"
+  },
+  {
+    "revision": "2ed3f71d9a4d246a3bfc",
+    "url": "/static/css/main.426a665c.chunk.css"
+  },
+  {
+    "revision": "a5a7da2dea8bebee0a04",
+    "url": "/static/js/2.a5774323.chunk.js"
+  },
+  {
+    "revision": "7080545ef704c06e1e8d2c1ff7c39799",
+    "url": "/static/js/2.a5774323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed3f71d9a4d246a3bfc",
+    "url": "/static/js/main.93316b13.chunk.js"
+  },
+  {
+    "revision": "c53b3e18d526df4231ee",
+    "url": "/static/js/runtime-main.4e8030d8.js"
+  },
+  {
+    "revision": "c9d28f270443412ba2571d5b43c2ff9c",
+    "url": "/static/media/1.c9d28f27.jpg"
+  }
+]);
