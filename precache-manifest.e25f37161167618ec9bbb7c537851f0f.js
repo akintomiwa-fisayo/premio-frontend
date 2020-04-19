808e6b061c7930cf6ac8418a6824bf4c50c20c48
akintomiwa fisayo <akintomiwa.fisayo@gmail.com>
2020-04-19 17:36:55 +0100

    Updates

--- precache-manifest.e25f37161167618ec9bbb7c537851f0f.js
+++ precache-manifest.e25f37161167618ec9bbb7c537851f0f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c62e606145e57ed3b6e2adb82061cc9",
+    "revision": "e88797c7d04b09cb897f7047bef1930c",
     "url": "/premio-frontend/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/premio-frontend/static/css/2.54e6f0dc.chunk.css"
   },
   {
-    "revision": "ff00e68a50e64815487e",
+    "revision": "a68e888bcb4f501f1360",
     "url": "/premio-frontend/static/css/main.cdbb4fe3.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/premio-frontend/static/js/2.157f158a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ff00e68a50e64815487e",
-    "url": "/premio-frontend/static/js/main.75276994.chunk.js"
+    "revision": "a68e888bcb4f501f1360",
+    "url": "/premio-frontend/static/js/main.2ffbd720.chunk.js"
   },
   {
     "revision": "62a4ecef531e31e08a14",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/premio-frontend/precache-manifest.513a91099e358306676fbfd594561918.js"
+  "/premio-frontend/precache-manifest.e25f37161167618ec9bbb7c537851f0f.js"
 );
 
 self.addEventListener('message', (event) => {
